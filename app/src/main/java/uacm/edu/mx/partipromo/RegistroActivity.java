2c3222e1695448b0be4735f58b4905f4b26a3df9
MartinezYonathan <j-ma@hotmail.es>
2020-01-09 08:52:58 -0600

    se esta integrando api de firebase

--- app/src/main/java/uacm/edu/mx/partipromo/RegistroActivity.java
+++ app/src/main/java/uacm/edu/mx/partipromo/RegistroActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import uacm.edu.mx.partipromo.domain.Participante;
+
 public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
     }
 }
