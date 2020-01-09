959bea12a944ee9c2a999252839fe7b229855e62
fernanda-rosales <chica_priston066hotmail.com>
2020-01-09 05:35:11 -0600

    se corrijen los activitys layouts

---
+++ app/src/main/java/uacm/edu/mx/partipromo/PromoprincipalActivity.java
@@ -0,0 +1,14 @@
+package uacm.edu.mx.partipromo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PromoprincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promoprincipal);
+    }
+}
